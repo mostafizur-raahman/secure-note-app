37442641cf869efa1aec52710cd54906910eaa70
Mostafizur Rahman <mostafizurrahman0202@gmail.com>
2026-07-25 17:05:27 +0600

    feat: project set up initial stage done

---
+++ src/server.js
@@ -0,0 +1,18 @@
+import http from "http";
+import app from "./app.js";
+import config from "./config/envConfig.js";
+
+const PORT = config.PORT;
+
+const server = http.createServer(app);
+
+server.listen(PORT, "0.0.0.0", () => {
+    console.log(`(PID: ${process.pid}) listening on port ${PORT}`);
+});
+
+// use for memeory leak
+process.on("SIGTERM", () => {
+    server.close(() => {
+        process.exit(0);
+    });
+});
